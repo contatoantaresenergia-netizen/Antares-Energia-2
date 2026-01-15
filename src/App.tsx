96424d8604ff2be13664c5eaaefe7b6ceb7028dc
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-01-15 13:10:53 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+
+import React, { useState, useEffect } from 'react';
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Services } from './pages/Services';
+import { Projects } from './pages/Projects';
+import { Contact } from './pages/Contact';
+
+// Scroll to top component
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+const App: React.FC = () => {
+  return (
+    <HashRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="sobre" element={<About />} />
+          <Route path="servicos" element={<Services />} />
+          <Route path="projetos" element={<Projects />} />
+          <Route path="contato" element={<Contact />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
+};
+
+export default App;
