ae208f7002a95b01807134621b0b8d0ac1f6315d
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-01-16 11:18:00 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,17 +19,17 @@ export default {
       fontSize: {
         'xs': ['0.75rem', { lineHeight: '1rem' }],
         'sm': ['0.875rem', { lineHeight: '1.25rem' }],
-        'base': ['1rem', { lineHeight: '1.5rem' }],
+        'base': ['1rem', { lineHeight: '1.5rem' }], 
         'lg': ['1.125rem', { lineHeight: '1.75rem' }],
         'xl': ['1.25rem', { lineHeight: '1.75rem' }],
         '2xl': ['1.5rem', { lineHeight: '2rem' }],
         '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
         '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
         '5xl': ['3rem', { lineHeight: '1.2' }],
-        '6xl': ['3.5rem', { lineHeight: '1.1' }], // Ajustado levemente
-        '7xl': ['4rem', { lineHeight: '1' }],   // Ajustado levemente
-        '8xl': ['5.5rem', { lineHeight: '1' }], // Ajustado para evitar atropelar o header
-        '9xl': ['7rem', { lineHeight: '1' }],   // Ajustado
+        '6xl': ['3.5rem', { lineHeight: '1.1' }], 
+        '7xl': ['4.2rem', { lineHeight: '1' }],   
+        '8xl': ['5rem', { lineHeight: '1' }],   
+        '9xl': ['6.5rem', { lineHeight: '1' }], 
       },
       maxWidth: {
         '8xl': '88rem',
