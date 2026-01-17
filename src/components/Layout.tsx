31d0d0d92bace53cda4308f0993f211d8246a18f
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-01-17 18:54:39 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Header } from './Header';
+import { Footer } from './Footer';
 
 export const Layout: React.FC = () => {
   return (
-    <div className="min-h-screen bg-black">
-      {/* Removemos a importação do Hero daqui para o build passar agora */}
-      <main>
+    <div className="min-h-screen bg-black flex flex-col">
+      <Header />
+
+      <main className="flex-1">
         <Outlet />
       </main>
+
+      <Footer />
     </div>
   );
 };
