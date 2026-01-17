91f4cbe9df60827b82c2fb235052d7f4dd10c776
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-01-17 14:12:14 +0100

    Create smoothScroll.ts

---
+++ src/utils/smoothScroll.ts
@@ -0,0 +1,27 @@
+import Lenis from '@studio-freight/lenis';
+import { gsap } from 'gsap';
+
+export const initSmoothScroll = () => {
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    smooth: true,
+    smoothTouch: false,
+  });
+
+  function raf(time: number) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+
+  // Sincroniza com GSAP
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+
+  return lenis;
+};
