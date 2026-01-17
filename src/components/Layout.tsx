36ad9a8bc1b378e313d274560aa8bc2d919ee1c6
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-01-17 17:47:47 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+// Importe o seu componente de navegação aqui (ajuste o nome se for Header ou Navbar)
+import { Hero } from './Hero'; 
 
 export const Layout: React.FC = () => {
   return (
     <div className="min-h-screen bg-black">
-      {/* O Outlet é onde as páginas (Home, Sobre, etc) aparecem */}
+      {/* O Header deve ficar aqui para aparecer em todas as páginas */}
+      <header>
+         {/* Se você tiver um componente <Navbar />, coloque-o aqui */}
+      </header>
+      
       <main>
-        <Outlet />
+        <Outlet /> {/* As páginas aparecem aqui */}
       </main>
+
+      {/* Se quiser um Footer fixo, coloque-o aqui embaixo */}
     </div>
   );
 };
