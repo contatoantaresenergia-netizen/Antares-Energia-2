0b319a76571bfcceb9994b357443a0ac49fd2c4f
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-01-17 15:10:31 +0100

    Update useAnimations.ts

--- src/hooks/useAnimations.ts
+++ src/hooks/useAnimations.ts
@@ -1,29 +1,8 @@
 import { useEffect } from 'react';
-import {
-  initHeroAnimations,
-  initStatsAnimation,
-  initCardsAnimation,
-  initMagneticButtons,
-} from '../utils/animations';
 
 export const useAnimations = () => {
   useEffect(() => {
-    const isMobile = window.innerWidth < 768;
-    
-    // Pequeno delay para garantir que o DOM está pronto
-    const timer = setTimeout(() => {
-      initHeroAnimations();
-      initStatsAnimation();
-      initCardsAnimation();
-      
-      if (!isMobile) {
-        initMagneticButtons();
-      }
-    }, 100);
-    
-    // Cleanup
-    return () => {
-      clearTimeout(timer);
-    };
+    // Animações desabilitadas temporariamente
+    console.log('Animations disabled');
   }, []);
 };
