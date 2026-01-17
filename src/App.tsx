cb82de11c4c03d865161ee0d6397a277a51b9164
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-01-17 17:19:52 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { useEffect } from 'react';
 import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
 
-// Importações ajustadas para os caminhos das suas pastas
-import { Layout } from './components/Layout'; 
+// Importações corrigidas com as chaves { }
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Services } from './pages/Services';
 import { Projects } from './pages/Projects';
 import { Contact } from './pages/Contact';
+
+// Se o Layout.tsx estiver dentro de 'components', use a linha abaixo. 
+// Se der erro, verifique se o arquivo Layout.tsx realmente existe nessa pasta.
+import { Layout } from './components/Layout'; 
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
