a712c856df667190e5c88acc21c52006a2067fd3
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-01-18 13:01:29 +0100

    Create Logo.tsx

---
+++ src/components/Logo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export interface LogoProps {
+  className?: string;
+  variant?: string; 
+}
+
+export const Logo: React.FC<LogoProps> = ({ className, variant }) => {
+  return (
+    <div className={className}>
+      {/* O texto ou SVG do seu logo aqui */}
+      <span style={{ color: variant === 'white' ? '#FFF' : 'inherit' }}>ANTARES</span>
+    </div>
+  );
+};
