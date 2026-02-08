2f715c50da7896a46e86a26c3d9ba88a6580a7e7
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-02-08 18:40:26 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import Home from './pages/Home';
-import Services from './pages/Services';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; 
 
-function App() {
-  return (
-    <HelmetProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-        </Routes>
-      </Router>
-    </HelmetProvider>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
