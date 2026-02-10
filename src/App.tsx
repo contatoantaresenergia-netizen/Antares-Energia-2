25ad45d7ea27b8992d58a7e1ef6640a9c5bad982
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-02-10 18:56:38 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async'; // 1. IMPORTAÇÃO ESSENCIAL
 import { Layout } from './components/Layout';
 
-// IMPORTAÇÕES CORRIGIDAS (Sem chaves para os componentes que são Default)
+// IMPORTAÇÕES CORRIGIDAS
 import Home from './pages/Home'; 
 import Camaragibe from './pages/Camaragibe';
 
-// IMPORTAÇÕES COM CHAVES (Para os componentes que são Named Exports)
+// IMPORTAÇÕES COM CHAVES
 import { About } from './pages/About';
 import { Services } from './pages/Services';
 import { Projects } from './pages/Projects';
@@ -27,29 +28,32 @@ const ScrollToTop: React.FC = () => {
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* ROTAS PADRÃO */}
-          <Route index element={<Home />} />
-          <Route path="sobre" element={<About />} />
-          <Route path="servicos" element={<Services />} />
-          <Route path="projetos" element={<Projects />} />
-          <Route path="contato" element={<Contact />} />
-          
-          {/* ROTAS DE CAMARAGIBE */}
-          <Route path="camaragibe" element={<Camaragibe />} />
-          <Route path="camaragibe/sobre" element={<CamaragibeSobre />} />
-          <Route path="camaragibe/servicos" element={<CamaragibeServices />} />
-          <Route path="camaragibe/projetos" element={<CamaragibeProjects />} />
-          <Route path="camaragibe/contato" element={<CamaragibeContact />} />
+    // 2. O HelmetProvider PRECISA envolver toda a aplicação
+    <HelmetProvider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* ROTAS PADRÃO */}
+            <Route index element={<Home />} />
+            <Route path="sobre" element={<About />} />
+            <Route path="servicos" element={<Services />} />
+            <Route path="projetos" element={<Projects />} />
+            <Route path="contato" element={<Contact />} />
+            
+            {/* ROTAS DE CAMARAGIBE */}
+            <Route path="camaragibe" element={<Camaragibe />} />
+            <Route path="camaragibe/sobre" element={<CamaragibeSobre />} />
+            <Route path="camaragibe/servicos" element={<CamaragibeServices />} />
+            <Route path="camaragibe/projetos" element={<CamaragibeProjects />} />
+            <Route path="camaragibe/contato" element={<CamaragibeContact />} />
 
-          {/* ROTA 404 */}
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+            {/* ROTA 404 */}
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 };
 
