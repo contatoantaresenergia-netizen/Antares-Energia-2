8988ae5d24d25a51a3c9bd9819f8c3f83e6e1870
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-02-10 22:21:44 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,19 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
 import { Layout } from './components/Layout';
-import { Home } from './pages/Home';          // ✅ nomeado
-import { Camaragibe } from './pages/Camaragibe'; // ✅ named import
-
+import { Home } from './pages/Home';           // named export
+import Camaragibe from './pages/Camaragibe';   // default export
 
 import { About } from './pages/About';
 import { Services } from './pages/Services';
 import { Projects } from './pages/Projects';
 import { Contact } from './pages/Contact';
-import { NotFound } from './pages/NotFound'; 
+import { NotFound } from './pages/NotFound';
 
-<Route path="camaragibe" element={<Camaragibe />} />
-<Route path="camaragibe/sobre" element={<CamaragibeSobre />} />
-<Route path="camaragibe/servicos" element={<CamaragibeServices />} />
-<Route path="camaragibe/projetos" element={<CamaragibeProjects />} />
-<Route path="camaragibe/contato" element={<CamaragibeContact />} />
-
+import { CamaragibeSobre } from './pages/CamaragibeSobre';
+import { CamaragibeServices } from './pages/CamaragibeServices';
+import { CamaragibeProjects } from './pages/CamaragibeProjects';
+import { CamaragibeContact } from './pages/CamaragibeContact';
 
 const ScrollToTop: React.FC = () => {
   const { pathname } = useLocation();
@@ -33,18 +30,21 @@ export default function App() {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
+          {/* Páginas principais */}
           <Route index element={<Home />} />
           <Route path="sobre" element={<About />} />
           <Route path="servicos" element={<Services />} />
           <Route path="projetos" element={<Projects />} />
           <Route path="contato" element={<Contact />} />
-          
-          <Route path="camaragibe" element={<div>Camaragibe</div>} />
+
+          {/* Camaragibe */}
+          <Route path="camaragibe" element={<Camaragibe />} />
           <Route path="camaragibe/sobre" element={<CamaragibeSobre />} />
           <Route path="camaragibe/servicos" element={<CamaragibeServices />} />
           <Route path="camaragibe/projetos" element={<CamaragibeProjects />} />
           <Route path="camaragibe/contato" element={<CamaragibeContact />} />
 
+          {/* Página 404 */}
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
