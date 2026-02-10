7e95040060448067486344748e47fd1701aa0c4b
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-02-10 19:04:26 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async'; // 1. IMPORTAÇÃO ESSENCIAL
+import { HelmetProvider } from 'react-helmet-async'; // Adicionado apenas para evitar erro
 import { Layout } from './components/Layout';
 
-// IMPORTAÇÕES CORRIGIDAS
+// IMPORTAÇÕES SEM CHAVES (Export Default)
 import Home from './pages/Home'; 
 import Camaragibe from './pages/Camaragibe';
 
-// IMPORTAÇÕES COM CHAVES
+// IMPORTAÇÕES COM CHAVES (Named Exports)
 import { About } from './pages/About';
 import { Services } from './pages/Services';
 import { Projects } from './pages/Projects';
@@ -16,7 +16,6 @@ import { CamaragibeSobre } from './pages/CamaragibeSobre';
 import { CamaragibeServices } from './pages/CamaragibeServices';
 import { CamaragibeProjects } from './pages/CamaragibeProjects';
 import { CamaragibeContact } from './pages/CamaragibeContact';
-import { NotFound } from './pages/NotFound';
 
 const ScrollToTop: React.FC = () => {
   const { pathname } = useLocation();
@@ -28,28 +27,22 @@ const ScrollToTop: React.FC = () => {
 
 const App: React.FC = () => {
   return (
-    // 2. O HelmetProvider PRECISA envolver toda a aplicação
-    <HelmetProvider>
+    <HelmetProvider> {/* Envolvendo para permitir o uso de SEO nas páginas sem quebrar o site */}
       <BrowserRouter>
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Layout />}>
-            {/* ROTAS PADRÃO */}
             <Route index element={<Home />} />
             <Route path="sobre" element={<About />} />
             <Route path="servicos" element={<Services />} />
             <Route path="projetos" element={<Projects />} />
             <Route path="contato" element={<Contact />} />
             
-            {/* ROTAS DE CAMARAGIBE */}
             <Route path="camaragibe" element={<Camaragibe />} />
             <Route path="camaragibe/sobre" element={<CamaragibeSobre />} />
             <Route path="camaragibe/servicos" element={<CamaragibeServices />} />
             <Route path="camaragibe/projetos" element={<CamaragibeProjects />} />
             <Route path="camaragibe/contato" element={<CamaragibeContact />} />
-
-            {/* ROTA 404 */}
-            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
