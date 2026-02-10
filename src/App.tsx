d34783a13bec7cc8049ed5fea238b4fbdc91cec3
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-02-10 22:24:39 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
+// Layout
 import { Layout } from './components/Layout';
-import { Home } from './pages/Home';           // named export
-import Camaragibe from './pages/Camaragibe';   // default export
 
+// Páginas principais
+import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Services } from './pages/Services';
 import { Projects } from './pages/Projects';
 import { Contact } from './pages/Contact';
 import { NotFound } from './pages/NotFound';
 
+// Páginas Camaragibe
+import { Camaragibe } from './pages/Camaragibe';
 import { CamaragibeSobre } from './pages/CamaragibeSobre';
 import { CamaragibeServices } from './pages/CamaragibeServices';
 import { CamaragibeProjects } from './pages/CamaragibeProjects';
 import { CamaragibeContact } from './pages/CamaragibeContact';
 
+// Componente para rolar para o topo a cada troca de rota
 const ScrollToTop: React.FC = () => {
   const { pathname } = useLocation();
   useEffect(() => {
@@ -30,14 +34,14 @@ export default function App() {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
-          {/* Páginas principais */}
+          {/* Rotas principais */}
           <Route index element={<Home />} />
           <Route path="sobre" element={<About />} />
           <Route path="servicos" element={<Services />} />
           <Route path="projetos" element={<Projects />} />
           <Route path="contato" element={<Contact />} />
 
-          {/* Camaragibe */}
+          {/* Rotas Camaragibe */}
           <Route path="camaragibe" element={<Camaragibe />} />
           <Route path="camaragibe/sobre" element={<CamaragibeSobre />} />
           <Route path="camaragibe/servicos" element={<CamaragibeServices />} />
