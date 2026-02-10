c4af0646d3301c798c772424f8efae1a541a8450
contatoantaresenergia-netizen <contatoantaresenergia@gmail.com>
2026-02-10 22:06:16 +0100

    Update Camaragibe.tsx

--- src/pages/Camaragibe.tsx
+++ src/pages/Camaragibe.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
+import { Outlet } from 'react-router-dom';
 
-const Camaragibe: React.FC = () => {
-  return (
-    <main className="bg-white min-h-screen p-10">
-      <h1 className="text-4xl font-bold mb-6 text-black">Energia Solar em Camaragibe</h1>
-      <p className="text-lg text-gray-700">
-        Conteúdo seguro do site. Se isso aparecer, o problema é no Typewriter ou GSAP.
-      </p>
+export const Layout: React.FC = () => (
+  <>
+    <header>…</header>
+    <main>
+      <Outlet />  {/* <- É aqui que o Camaragibe, Home, etc. vão aparecer */}
     </main>
-  );
-};
-
-export default Camaragibe;
+    <footer>…</footer>
+  </>
+);
